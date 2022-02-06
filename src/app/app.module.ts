52f7b06b71b46a4123085f3413068e853324099d
Dustin Keyes <dustin.keyes@gmail.com>
2022-02-05 21:05:52 -0600

    adding email form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { FirstCompComponent } from './first-comp/first-comp.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EmailFormComponent } from './email-form/email-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstCompComponent
+    FirstCompComponent,
+    EmailFormComponent
   ],
   imports: [
     BrowserModule,
