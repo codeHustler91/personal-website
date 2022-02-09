4eb5b7c9fe3e584fbda4fa30337559dc8bdc66df
Dustin Keyes <dustin.keyes@gmail.com>
2022-02-08 23:15:42 -0600

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dk-website';
+
+  themeColor = "grey";
+
+  updateThemeColor() {
+    this.themeColor = event.target.value;
+  }
+
 }
