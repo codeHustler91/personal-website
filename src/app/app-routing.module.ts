a2a859866e9950e84c75118dc62b9e0557d8a495
Dustin Keyes <dustin.keyes@gmail.com>
2022-02-14 17:39:40 -0600

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularTutorial1Component } from '../app/angular-tutorial1/angular-tutorial1.component';
+import { RelationshipBuilderComponent } from '../app/relationship-builder/relationship-builder.component';
+import { EmailFormComponent } from '../app/email-form/email-form.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'tutorial', component: AngularTutorial1Component },
+  { path: 'relationship', component: RelationshipBuilderComponent },
+  { path: 'email_form', component: EmailFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
